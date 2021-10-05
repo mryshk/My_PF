93419cc4cf7cbc3b504d1c7c0d9505b9b58e1e0c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-05 12:16:11 +0000

    Add All Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,42 @@
 Rails.application.routes.draw do
-  devise_for :listeners
   root to: 'homes#top'
+  get 'about' => 'homes#about',as: 'about'
   
+  devise_for :listeners
   resources :listeners, only: [:show,:edit,:update] do
     resource :relationships, only:[:create,:destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers',as: 'followers'
   end
+  resources :posts do
+    collection do
+      get :order
+      get :search
+      get :search_tag
+    end
+    resources :post_comments, only: [:index,:create,:edit,:update,:destroy]
+    resource :post_favorites, only: [:index,:create,:destroy]
+  end
+  
+  resources :groups do
+    resources :group_listeners, only: [:create, :index, :destroy]
+  end
+  
+  resources :chats, only: [:create,:show,:index]
+  
+  get 'notification' => 'notifications#index',as: 'notifications'
+  
+  resources :albums, only: [:index,:show]do
+    resources :album_musics, only: [:index,:show]do
+      resources :music_comments, only: [:index,:create,:edit,:update,:destroy]
+      resources :music_favorites, only: [:index,:create,:destroy]
+    end
+  end
+  
+  resources :inquiries, only: [:new,:create,:finish]
   
   
- 
- namespace :artist do
+  namespace :artist do
    devise_for :creaters
    resources :creaters, only: [:show,:edit,:update]
    resources :albums do
@@ -19,4 +45,12 @@ Rails.application.routes.draw do
      end
    end
  end
+
+  
+  
 end
+  
+  
+  
+ 
+
