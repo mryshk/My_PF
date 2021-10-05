ba4ca43caee6eac2ef34de50dd43ee4edbbcb873
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-05 08:24:38 +0000

    Add column to creaters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_080756) do
+ActiveRecord::Schema.define(version: 2021_10_05_081530) do
 
   create_table "album_musics", force: :cascade do |t|
     t.integer "creater_id"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 2021_10_05_080756) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "caption"
+    t.string "profile_image_id"
+    t.string "creater_url"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_creaters_on_email", unique: true
     t.index ["reset_password_token"], name: "index_creaters_on_reset_password_token", unique: true
   end
