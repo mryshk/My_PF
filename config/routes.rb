0c9e7f49785f9f5b77810db75bee42da72de12ac
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-05 11:46:29 +0000

    Add artist routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,16 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers',as: 'followers'
   end
+  
+  
  
  namespace :artist do
    devise_for :creaters
-   
+   resources :creaters, only: [:show,:edit,:update]
+   resources :albums do
+     resources :album_musics do
+       resources :music_comments, only: [:index]
+     end
+   end
  end
 end
