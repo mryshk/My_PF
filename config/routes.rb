1d0e8cdffe0fc0ef5e8f28773406d9bbdc9e5cbc
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-05 11:42:05 +0000

    Add listeners routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :creaters
   devise_for :listeners
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  
+  resources :listeners, only: [:show,:edit,:update] do
+    resource :relationships, only:[:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers',as: 'followers'
+  end
+ 
+ namespace :artist do
+   devise_for :creaters
+   
+ end
 end
