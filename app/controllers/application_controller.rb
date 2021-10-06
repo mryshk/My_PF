fdc0f34302d326569e43d81f55339270e7c61b0f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-06 01:37:16 +0000

    Modify apprication.controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    if listener_signed_in?
-      public_root_path
+    if listener_signed_in? #リスナー側ログイン後
+      public_root_path #リスナー側トップ画面に遷移
     else
-      artist_root_path
+      artist_root_path #クリエイター側ログイン後はマイページに遷移
     end
   end
 
-  def after_sign_out_path_for(resource)
-    public_root_path
+  def after_sign_out_path_for(resource) #リスナー側ログアウト後
+    about_path #About画面に遷移
   end
 
 end
