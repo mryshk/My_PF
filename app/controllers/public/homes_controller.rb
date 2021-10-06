7496abe7d16459f9364589ef54c96b1452722ca3
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-06 07:24:42 +0000

    Add some controllers

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,10 @@
 class Public::HomesController < ApplicationController
+  # フォローしている人のみを表示。タイムライン機能。
+
+  def top
+    @Posts = Post.where(user_id:[current_user,*current_user.following_ids]).page(params[:page]).reverse_order
+  end
+
+  def about
+  end
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,50 @@
 class Public::PostsController < ApplicationController
+    def  new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @poat.user_id = current_listener.id
+        @post.save
+        redirect_to post_path(@post)
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        @post_comment_n = PostComment.new
+        @comments = @post.post_comments
+    end
+
+    def index
+        @posts = Post.page(params[:page]).reverse_order
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.listenr_id = current_listener.id
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+
+    def search
+        @search = Post.search(params[:keyword]).page(params[:page]).reverse_order
+        @keyword = params[:keyword]
+        render "search"
+    end
+    
+   private
+   def music_params
+       params.require(:music).permit(:musicimage,:music_name,:artist_name,:music_caption,:music_url,:music_genre)
+   end
 end
