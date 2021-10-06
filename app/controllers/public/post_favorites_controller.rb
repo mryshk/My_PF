de0ee0f81c4d776883082164be9e8619d2a87cdd
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-06 14:42:27 +0000

    save

--- app/controllers/public/post_favorites_controller.rb
+++ app/controllers/public/post_favorites_controller.rb
@@ -2,13 +2,13 @@ class Public::PostFavoritesController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        @favorite = current_listener.favorites.new(post_id: @post.id)
+        @favorite = current_listener.post_favorites.new(post_id: @post.id)
         @favorite.save
     end
 
     def destroy
         @post = Post.find(params[:post_id])
-        @favorite = current_listener.favorites.find_by(post_id: @post.id)
+        @favorite = current_listener.post_favorites.find_by(post_id: @post.id)
         @favorite.destroy
     end
 end
