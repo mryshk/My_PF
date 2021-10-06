7e871d7a4273a38853a0c5b1cb7b2f5a7576f38f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-06 06:49:17 +0000

    save

--- app/controllers/public/listeners/omniauth_callbacks_controller.rb
+++ app/controllers/public/listeners/omniauth_callbacks_controller.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 class Public::Listeners::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-  protect_from_forgery
+  protect_from_forgery #ActionController::InvalidAuthenticityTokenエラー対策
 
   def google_oauth2
     callback_for(:google)
@@ -23,23 +18,5 @@ class Public::Listeners::OmniauthCallbacksController < Devise::OmniauthCallbacks
   def failure
     redirect_to root_path
   end
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
