f9f312bce8e15390de6bf852e2892c7f9e3fea7b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-09 12:12:18 +0000

    modify model name

--- spec/models/group_listener_spec.rb
+++ spec/models/group_listener_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe GroupLitener, "モデルに関するテスト", type: :model do
+RSpec.describe GroupListener, "モデルに関するテスト", type: :model do
   let (:group_listener) { FactoryBot.create(:group_listener) }
   describe '実際に保存してみる' do
     it "有効な投稿内容の場合は保存されるか" do
