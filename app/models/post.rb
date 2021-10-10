a671e74e929c186125026cefe894a50ca541c171
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-10 01:13:48 +0000

    Add comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,28 +7,31 @@ class Post < ApplicationRecord
   has_many :tagmaps, dependent: :destroy
   has_many :tags, through: :tagmaps
 
+  # ファボモデルに自分が存在するかどうか。していたら削除。なかったら作成。
   def favorited_by?(listener)
     post_favorites.where(listener_id: listener.id).exists?
   end
-
+　# 検索機能。投稿内容のキーワードで検索。前後関係なしにどれか一致すればヒットする。
   def self.search(keyword)
     where(["post_tweet like?", "%#{keyword}%"])
   end
-  
+　# 並び替え機能。プルダウンで取得した用語で並び替え内容を場合分けしている。
   def self.sort(order)
     case order
+    # 新しい順
     when 'new'
       return all.order(created_at: :DESC)
-    
+    # 古い順
     when 'old'
       return all.order(creater_at: :ASC)
-    
+    # いいねが多い順
     when 'likes'
       return find(PostFavorite.group(:post_id).order(Arel.sql('count(post_id) desc')).pluck(:post_id))
-    end 
+    end
   end
-  
+
+  # 閲覧数機能の許可
   is_impressionable counter_cache: true
-  
-  
+
+
 end
