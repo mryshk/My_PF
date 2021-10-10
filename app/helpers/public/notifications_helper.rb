0be146109ca234295dc1775388da3b4f448d4e21
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-10 14:06:58 +0000

    Add unchecked_notifications

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -1,2 +1,23 @@
 module Public::NotificationsHelper
+  def notification_form(notification)
+    @active = notification.active
+    @comment = nil
+    your_post = link_to 'あなたの投稿', post_path(notification), style: "font-weight: bold;"
+    @active_comment = notification.post_comment_id
+
+    case notification.action
+      when "follow" then
+        tag.a(notification.active.name, href: listener_path(@active), style: "font-weight: bold;")+"があなたをフォローしました。"
+      when "like" then
+        tag.a(notification.active.name, href: listener_path(@active), style: "font-weight: bold;")+"が"+tag.a('あなた投稿を', href:post_path(notification.post_id), style: "font-weight: bold;")+"いいねしました。"
+      when "comment" then
+        @comment = PostComment.find_by(id: @active_comment)&.comment
+        tag.a(@active.name, href:listener_path(@active), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にコメントしました"
+    end
+  end
+
+  def unchecked_notifications
+    @notifications = current_listener.passive_notifications.where(checked: false)
+  end
+
 end
