88fe61f8e3225985bd823fea1bb1b8978285aa09
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-10 12:54:28 +0000

    modify create_action

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,2 +1,13 @@
 class Public::NotificationsController < ApplicationController
+  def index
+    @notifications = current_listener.passive_notifications
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+
+  def destroy_all
+    @notifications = current_listener.passive_notifications.destroy_all
+    redirect_to notifications_path
+  end
 end
