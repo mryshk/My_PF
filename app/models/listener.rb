daa1edcdb31217d44418affa28b89790417b0433
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-10 12:55:27 +0000

    Add notificartion action

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -41,7 +41,7 @@ class Listener < ApplicationRecord
 
   # ログイン中のリスナーを引数で取得し、そのリスナーがフォローした人としてリレーションシップに登録される。
   def follow(listener_id)
-    relationships.create(followed_id: listener_id)
+    relationships.create!(followed_id: listener_id)
   end
 
   def unfollow(listener_id) # リレーションシップから引数のリスナーを削除する。
@@ -59,4 +59,14 @@ class Listener < ApplicationRecord
   # 閲覧数機能の許可
   is_impressionable counter_cache: true
 
+  def create_notification_follow!(current_listener)
+    temp = Notification.where(["passive_id = ? and active_id = ? and action = ?", current_listener.id, id, 'follow'])
+    if temp.blank?
+      notification = current_listener.active_notifications.new(
+        passive_id: id,
+        action: 'follow'
+        )
+        notification.save if notification.valid?
+    end
+  end
 end
