8d35d63fb5d5a7f18d60220a20b01cc7c0dc4cdd
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-10 07:49:43 +0000

    Add define save_tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -34,16 +34,16 @@ class Post < ApplicationRecord
   is_impressionable counter_cache: true
 
   def save_tag(sent_tags)
-    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
     old_tags = current_tags - sent_tags
     new_tags = sent_tags - current_tags
 
     old_tags.each do |old|
-      self.post_tags.delete Tag.find_by(name: old)
+      self.tags.delete Tag.find_by(tag_name: old)
     end
 
     new_tags.each do |new|
-      new_post_tag = Tag.find_or_create_by(name: new)
+      new_post_tag = Tag.find_or_create_by(tag_name: new)
       self.tags << new_post_tag
     end
 
