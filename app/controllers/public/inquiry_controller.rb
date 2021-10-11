90bbb2354e902d111a9b3ea509ae91dec1fc5d90
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 07:43:56 +0000

    Add inquiry

--- app/controllers/public/inquiry_controller.rb
+++ app/controllers/public/inquiry_controller.rb
@@ -1,10 +1,25 @@
 class Public::InquiryController < ApplicationController
   def new
+    @inquiry = Inquiry.new
   end
 
   def confirm
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.valid?
+      render 'confirm'
+    else
+      render 'new'
+    end
   end
 
   def finish
+    @inquiry = Inquiry.new(inquiry_params)
+    InquiryMailer.received_email(@inquiry).deliver
+    render 'finish'
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:name, :email, :message)
   end
 end
