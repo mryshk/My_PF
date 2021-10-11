598398f08f32de790c0ae1a2009c07322972a9b8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 01:22:19 +0000

    Modify

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -2,7 +2,7 @@ class Listener < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable,:validatable,
          :omniauthable, omniauth_providers: [:google_oauth2, :twitter, :facebook]
 
   def self.from_omniauth(auth)
