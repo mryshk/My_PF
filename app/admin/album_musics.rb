7d0fe48d828e9a9c7aca6fa4347bd28daf8e5513
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 05:25:26 +0000

    Add permit_column to active_admin

--- app/admin/album_musics.rb
+++ app/admin/album_musics.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register AlbumMusic do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :creater_id, :album_id, :name, :caption, :music_url
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:creater_id, :album_id, :name, :caption, :music_url]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :creater_id, :album_id, :name, :caption, :music_url
 end
--- app/admin/notifications.rb
+++ app/admin/notifications.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Notification do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :post_id, :active_id, :passive_id, :post_comment_id, :action, :checked
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:post_id, :active_id, :passive_id, :post_comment_id, :action, :checked]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :post_id, :active_id, :passive_id, :post_comment_id, :action, :checked
 end
