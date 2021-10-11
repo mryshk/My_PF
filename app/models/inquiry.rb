dda4199ac33465d0984d434735715ebab6922d3b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 07:44:30 +0000

    Add inquiry model&teble

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,9 +1,3 @@
-class Inquiry
-  include ActiveModel::Model
-
-  attr_accessor :name, :email, :message
-
-  validates :name, :presense => { :message => "名前を入力してください。" }
-  validates :email, :presense => { :message => "メールアドレスを入力してください。" }
-
+class Inquiry < ApplicationRecord
+  
 end
