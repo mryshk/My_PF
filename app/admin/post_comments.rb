10e0b33dbfeb453274ef819408d60cf2cb663273
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 03:20:09 +0000

    Add active_admin

--- app/admin/post_comments.rb
+++ app/admin/post_comments.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register PostComment do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :listener_id, :post_id, :comment
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:listener_id, :post_id, :comment]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :listener_id, :post_id, :comment
 end
