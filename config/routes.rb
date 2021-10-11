d816d6e9eb0b1a4bc8e8ca771e75e15e918b57e8
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 06:42:43 +0000

    delete for spellmiss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   # 以下リスナー側ルート
   # コントローラー指定にpublicを追加。URLはpublic記述なし。(module)
