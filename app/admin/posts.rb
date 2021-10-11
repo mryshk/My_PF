32d62e19f4ac70c47a888d3541e517261d0aeced
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-11 02:19:20 +0000

    Add admin post

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Post do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :listener_id, :post_url, :post_tweet, :picture_id, :impressions_count
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:listener_id, :post_url, :post_tweet, :picture_id, :impressions_count]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  post_params :listener_id, :post_url, :post_tweet, :picture_id, :impressions_count
 end
