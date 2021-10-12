f37e07965e2ae871e609991a3ea066d3730e30ed
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-12 07:27:27 +0000

    Add listener_type and creater

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_074049) do
+ActiveRecord::Schema.define(version: 2021_10_12_055258) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -68,21 +68,10 @@ ActiveRecord::Schema.define(version: 2021_10_11_074049) do
   end
 
   create_table "creaters", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "caption"
-    t.string "profile_image_id"
-    t.string "creater_url"
-    t.string "uid"
-    t.string "provider"
-    t.index ["email"], name: "index_creaters_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_creaters_on_reset_password_token", unique: true
+    t.integer "lisntener_id"
+    t.integer "listener_id"
   end
 
   create_table "group_listeners", force: :cascade do |t|
@@ -156,6 +145,7 @@ ActiveRecord::Schema.define(version: 2021_10_11_074049) do
     t.string "uid"
     t.string "provider"
     t.integer "impressions_count", default: 0
+    t.integer "listener_type"
     t.index ["email"], name: "index_listeners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_listeners_on_reset_password_token", unique: true
   end
