783d717dd076e1b7da3a000a8557b3402d31fd0f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-12 07:24:45 +0000

    Add creter_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if listener_signed_in? # リスナー側ログイン後
       root_path # リスナー側トップ画面に遷移
-    else
-      artist_root_path # クリエイター側ログイン後はマイページに遷移
     end
   end
 
