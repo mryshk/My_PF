d8ee7feea4bafe1853e7dea1bbd14b7b5211152c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-12 00:22:30 +0000

    Add creater

---
+++ app/controllers/artist/creaters_controller.rb
@@ -0,0 +1,16 @@
+class Artist::CreatersController < ApplicationController
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+
+end
---
+++ test/controllers/artist/creaters_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Artist::CreatersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
