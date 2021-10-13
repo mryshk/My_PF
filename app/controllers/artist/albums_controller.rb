d14359b8fb2d3dc59d92da722d9297abbc1ab731
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-13 02:29:46 +0000

    Add action

--- app/controllers/artist/albums_controller.rb
+++ app/controllers/artist/albums_controller.rb
@@ -1,2 +1,39 @@
 class Artist::AlbumsController < ApplicationController
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    @album.creater_id = current_listener.creater.id
+    @album.save
+    redirect_to artist_album_path(@album)
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    @albums = Album.page(params[:page])
+  end
+
+  def index
+    @albums = Album.page(params[:page])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:name, :caption, :album_image_id, :genre, :album_url)
+  end
 end
