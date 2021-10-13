85e32cfeb9cd882dc7959d48b12d6aeef14eb388
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-13 02:29:37 +0000

    Add action

--- app/controllers/artist/album_musics_controller.rb
+++ app/controllers/artist/album_musics_controller.rb
@@ -1,2 +1,32 @@
 class Artist::AlbumMusicsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def album_music_params
+    params.require(:album_music).permit(:name, :caption, :music_url)
+  end
+
+
 end
