03ccb4ac95a82385349ccd3b6cafd183bc3b5356
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-13 06:05:41 +0000

    Add album__music_action

--- app/controllers/artist/album_musics_controller.rb
+++ app/controllers/artist/album_musics_controller.rb
@@ -1,24 +1,38 @@
 class Artist::AlbumMusicsController < ApplicationController
 
   def new
+    @album_music = AlbumMusic.new
   end
 
   def create
+     @album_music = AlbumMusic.new(album_music_params)
+     @album_music.save
+     redirect_to artist_album_album_music_path(@album_music)
   end
 
   def show
+    @album_music = AlbumMusic.find(params[:id])
+
   end
 
   def index
+    @album_musics = AlbumMusic.page(params[:page])
   end
 
   def edit
+     @album_music = AlbumMusic.new
   end
 
   def update
+    @album_music = AlbumMusic.find(params[:id])
+    @album_music.update(album_music_params)
+    redirect_to artist_album_album_music_path(@album_music)
   end
 
   def destroy
+    @album_music = AlbumMusic.find(params[:id])
+    @album_music = AlbumMusic.destroy
+    redirect_to artist_album_album_musics_path
   end
 
 
