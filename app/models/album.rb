18c327dd5ff2bf3fcbeedc9f47da50f502e907fb
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-13 02:30:23 +0000

    Add attachment and enum

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,6 +4,10 @@ class Album < ApplicationRecord
   has_many :music_comments, dependent: :destroy
   has_many :music_favorites, dependent: :destroy
 
+  attachment :album_image # 画像表示のため
+
+  enum genre: { ロック: 0, JPOP: 1, アイドル: 2, EDM: 3, KPOP: 4, パンク: 5, レゲエ: 6, HIPHOP: 7 }
+
   def favorited_by?(listener) # ファボをしているかどうかの確認・していれば削除へ
     music_favorites.where(listener_id: listener.id).exists?
   end
