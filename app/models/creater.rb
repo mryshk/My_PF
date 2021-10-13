7d85885cc5d793d6ac238c59635b751bf86fd791
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-13 02:51:48 +0000

    modify album_association

--- app/models/creater.rb
+++ app/models/creater.rb
@@ -1,7 +1,7 @@
 class Creater < ApplicationRecord
 
   belongs_to :listener
-  has_many :album, dependent: :destroy
+  has_many :albums, dependent: :destroy
   has_many :album_musics, dependent: :destroy
   has_many :music_comments, dependent: :destroy
   has_many :music_favorites, dependent: :destroy
