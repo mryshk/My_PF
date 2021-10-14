94f0d40c6ffd016ee37259cd95a1d9e0f4564c57
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-14 13:19:40 +0000

    Add index

--- app/controllers/public/post_favorites_controller.rb
+++ app/controllers/public/post_favorites_controller.rb
@@ -6,6 +6,11 @@ class Public::PostFavoritesController < ApplicationController
     @post.create_notification_by(current_listener)
   end
 
+  def index
+   @post = Post.find(params[:post_id])
+   @favorites = PostFavorite.where(post_id: @post.id)
+  end
+
   def destroy
     @post = Post.find(params[:post_id])
     @favorite = current_listener.post_favorites.find_by(post_id: @post.id)
