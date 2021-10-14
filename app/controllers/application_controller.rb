b1411b85dcaf85364babacda0985d51be894bd2f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-14 15:06:25 +0000

    Add cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception # ActionController::InvalidAuthenticityTokenエラー対策
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+   # 権限確認（cancancan）
+    def current_ability
+      @current_ability ||= ::Ability.new(current_listener)
+    end
+   # 権限確認後の遷移先（権限無しの場合）
+    rescue_from CanCan::AccessDenied do |_exception|
+     redirect_to root_path
+    end
+
 
   def after_sign_in_path_for(resource)
     if listener_signed_in? # リスナー側ログイン後
