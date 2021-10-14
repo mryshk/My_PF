c639f251b960d2d43b283ade98045f3c546c95d0
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-14 15:06:52 +0000

    Add cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(listener)
-    can :manage, :all
+
     listener ||= Listener.new
     if listener.listener_type == 0
+      can :read, Album
+      can :read, AlbumMusic
+
+    else
       can :manage, :all
-      cannot :create, Album
     end
   end
 end
