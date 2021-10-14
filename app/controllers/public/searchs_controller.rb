0a7cd2b06ca0adef238bb8cdf9f409dfde3f5da6
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-14 08:36:49 +0000

    Add search

---
+++ app/controllers/public/searchs_controller.rb
@@ -0,0 +1,19 @@
+class Public::SearchsController < ApplicationController
+  def search
+    @model = params["model"]
+    @keyword = params[:keyword]
+    @records = search_for(@model,@keyword).page(params[:page]).reverse_order
+  end
+
+  private
+
+  def search_for(model,keyword)
+    if model == "listener"
+      Listener.where(["name LIKE?", "%#{keyword}%"])
+    elsif model == "post"
+      Post.where(["post_tweet LIKE?","%#{keyword}%"] )
+    elsif model == "album"
+      Album.where(["name LIKE?","%#{keyword}%"] )
+    end
+  end
+end
