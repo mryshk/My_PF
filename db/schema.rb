ecac722afebc49daec0d0e9ce4d4bf4d24546e1b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-14 05:40:10 +0000

    Add group_chat_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_073929) do
+ActiveRecord::Schema.define(version: 2021_10_14_053908) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -71,6 +71,14 @@ ActiveRecord::Schema.define(version: 2021_10_13_073929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "listener_id"
+  end
+
+  create_table "group_chats", force: :cascade do |t|
+    t.integer "listener_id"
+    t.integer "group_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "group_listeners", force: :cascade do |t|
