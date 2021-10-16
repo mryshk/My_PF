7f38748162a9c21962b87489865f14e471f38949
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-16 12:04:37 +0000

    modify design

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,7 @@ class Public::HomesController < ApplicationController
   def top
     posts = Post.where(listener_id: [current_listener, *current_listener.following_ids])
     @posts = posts.page(params[:page]).reverse_order
-    @post_favorite_rank = Post.find(PostFavorite.group(:post_id).order('count(:post_id) desc').pluck(:post_id))
+    @post_favorite_rank = Post.includes(:favo_users).sort {|a,b| b.favo_users.size <=> a.favo_users.size}
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
   end
 
