4a00fcd803dce69c2a2cecfd113a9aca40a0c5bf
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-16 02:56:17 +0000

    Remove activeadmin_css from public

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -332,4 +332,9 @@ ActiveAdmin.setup do |config|
   # You can switch to using Webpacker here.
   #
   # config.use_webpacker = true
+
+config.clear_stylesheets!
+config.register_stylesheet "stylesheets/admin/active_admin.css"
+config.clear_javascripts!
+config.register_javascript "javascripts/admin/active_admin.js"
 end
