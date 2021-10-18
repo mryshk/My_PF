1aeca53d4cbf51297b95254b9e38f5c37eb33793
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-18 13:33:39 +0000

    add space

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
   # フォローしている人のみを表示。タイムライン機能。
 
   def top
-    posts = Post.all #where(listener_id: [current_listener, *current_listener.following_ids])
+    posts = Post.all  #where(listener_id: [current_listener, *current_listener.following_ids])
     @posts = posts.page(params[:page]).reverse_order
     @post_favorite_rank = Post.includes(:favo_users).sort {|a,b| b.favo_users.size <=> a.favo_users.size}
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
