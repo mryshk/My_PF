97dce8915c89727d2c489cf6775a2937e06c73f2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-19 03:53:28 +0000

    Add mygroup

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -10,6 +10,9 @@ class Public::HomesController < ApplicationController
     @posts = posts.page(params[:page]).reverse_order
     @post_favorite_rank = Post.includes(:favo_users).sort {|a,b| b.favo_users.size <=> a.favo_users.size}
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
+    # 自分の所属するグループを全て集める。
+    mygroup_ids = current_listener.group_listeners.pluck(:group_id)
+    @mygroups = Group.where(id: mygroup_ids)
   end
 
   def about
