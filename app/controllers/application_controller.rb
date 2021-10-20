894ecbc0a84c0d34b76a9855ff548dafd42b71e0
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-20 03:22:02 +0000

    no modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception # ActionController::InvalidAuthenticityTokenエラー対策
 
-  before_action :authenticate_listener!, except: [:top, :about]
+  before_action :authenticate_listener!,except: [:top,:about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # 権限確認（cancancan）
-  def current_ability
-    @current_ability ||= ::Ability.new(current_listener)
-  end
-  # 権限確認後の遷移先（権限無しの場合）
-  rescue_from CanCan::AccessDenied do |_exception|
-    redirect_to root_path, alert: '画面を閲覧する権限がありません。'
-  end
+   # 権限確認（cancancan）
+    def current_ability
+      @current_ability ||= ::Ability.new(current_listener)
+    end
+   # 権限確認後の遷移先（権限無しの場合）
+    rescue_from CanCan::AccessDenied do |_exception|
+     redirect_to root_path, alert: '画面を閲覧する権限がありません。'
+    end
+
 
   def after_sign_in_path_for(resource)
     if listener_signed_in? # リスナー側ログイン後
@@ -28,6 +29,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :listener_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name,:listener_type])
   end
+
+
 end
