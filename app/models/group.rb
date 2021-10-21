5a976434be6bbebd04cde1c81f797b518f50d649
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-21 02:32:38 +0000

    Add dependent:destroy

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   attachment :image
   has_many :group_listeners, dependent: :destroy
   has_many :group_chats, dependent: :destroy
-  has_many :listeners, through: :group_listeners
+  has_many :listeners, through: :group_listeners,dependent: :destroy
   validates :name, presence: true
 
   def joined_by?(listener_id)
