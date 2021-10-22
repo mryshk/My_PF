6d00dc847cbe7117952f998e0b103179803e4bff
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-22 14:52:12 +0000

    modify sentense

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -197,6 +197,6 @@ $(document).on('turbolinks:load', function() {
     // 次のページにいくためのリンクの場所は？ ＞aタグの指定
     nextSelector: 'a.next',
     // 読み込み中の表示はどうする？
-    loadingHtml: '読み込み中'
+    loadingHtml: 'Now loading'
   });
 });
