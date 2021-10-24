1427e3f66a08dd47a4b5f89d3a0c05123cfc590c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-24 03:18:13 +0000

    remove bullet at once

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
       enable_starttls_auto: true
   }
 
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-  end
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.alert = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.rails_logger = true
+  # end
 end
