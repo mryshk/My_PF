c4faad993724a2f166d55c91f130ad07e281baa1
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-26 05:15:17 +0000

    modify missing_word

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
   belongs_to :post, optional: true
-  belongs_to :post_commment, optional: true
+  belongs_to :post_comment, optional: true
   belongs_to :active, class_name: 'Listener', optional: true
   belongs_to :passive, class_name: 'Listener', optional: true
 end
