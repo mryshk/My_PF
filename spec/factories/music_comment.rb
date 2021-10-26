528f05be1afc49ce9b18153d621497dacc27fd13
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-26 03:39:24 +0000

    Add factorybot

---
+++ spec/factories/music_comment.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :music_comment do
+    association :listener
+    association :album
+    association :album_music
+    comment { Faker::Lorem.characters(number: 50) }
+    rate{ Faker::Number.between(from: 0.0, to: 5.0) }
+  end
+end
