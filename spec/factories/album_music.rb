61f687027f000e8a4fbf38c6bc460186aac76761
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-26 03:39:08 +0000

    Add factorybot

---
+++ spec/factories/album_music.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :album_music do
+    association :creater
+    association :album
+    name { Faker::Name.name }
+    caption { Faker::Lorem.characters(number: 50) }
+    music_url { Faker::Lorem.characters(number: 30) }
+  end
+end
