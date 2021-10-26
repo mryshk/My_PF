ff594e5615a317871eb06e20f454e0d49655fed6
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-26 03:38:10 +0000

    Add test_code

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     post_tweet { Faker::Lorem.characters(number: 30) }
     post_url { Faker::Lorem.characters(number: 10) }
     picture_id { Faker::Lorem.characters(number: 10) }
+    post_genre { Faker::Number.between(from: 0, to: 7) }
+    impressions_count {Faker::Number.number}
   end
 end
