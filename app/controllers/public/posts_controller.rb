5799de8a2a33917268f986d3b69bf21681f1da9f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-10-28 03:14:31 +0000

    Add order_ view

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -114,7 +114,6 @@ class Public::PostsController < ApplicationController
     # 自分の所属するグループを全て集める。
     mygroup_ids = current_listener.group_listeners.pluck(:group_id)
     @mygroups = Group.where(id: mygroup_ids)
-    render 'index'
   end
 
   private
