e46cf18d8e4449102c08a7e55066f2c053c01d5f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-02 11:49:41 +0000

    Add Natural API

--- app/controllers/artist/music_comments_controller.rb
+++ app/controllers/artist/music_comments_controller.rb
@@ -1,4 +1,5 @@
 class Artist::MusicCommentsController < ApplicationController
+
   def new
     @album = Album.find_by(id: params[:album_id])
     @album_music = AlbumMusic.find_by(id: params[:album_music_id])
@@ -11,6 +12,7 @@ class Artist::MusicCommentsController < ApplicationController
     @album_music = AlbumMusic.find_by(id: params[:album_music_id])
     @music_comment.album_id = @album.id
     @music_comment.album_music_id = @album_music.id
+    @music_comment.score = Language.get_data(music_comment_params[:comment])
     @music_comment.save!
     # create.js用
     @music_comments = @album_music.music_comments
