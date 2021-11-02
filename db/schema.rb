be8612802721aca2ee6ae1734c399b478e07246d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-02 11:50:08 +0000

    Add column to music_comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_031117) do
+ActiveRecord::Schema.define(version: 2021_11_02_074044) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_031117) do
     t.float "rate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "music_favorites", force: :cascade do |t|
