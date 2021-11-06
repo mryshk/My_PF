20db24616211eb4725c650ff977b0b3e7027085c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-06 13:45:38 +0000

    modify

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,6 +4,7 @@ class Public::PostCommentsController < ApplicationController
     @post_comment = PostComment.new
   end
 
+  # コメント作成
   def create
     @post = Post.find(params[:post_id])
     @comment = PostComment.new(post_comment_params)
@@ -14,7 +15,7 @@ class Public::PostCommentsController < ApplicationController
       @comment_post.create_notification_comment!(current_listener, @comment.id)
     end
     # create.js用
-    @comments = @post.post_comments
+    @comments = PostComment.where(post_id: @post.id, reply_comment: nil)
     @post_comment_n = PostComment.new
   end
 
@@ -25,16 +26,31 @@ class Public::PostCommentsController < ApplicationController
     @comments = PostComment.where(reply_comment: @post_comment.id)
   end
 
+  # 返信コメント作成
   def reply_create
     @post = Post.find(params[:post_id])
-    @post_comment = PostComment.find(params[:id])
     @comment = PostComment.new(post_comment_params)
     @comment.listener_id = current_listener.id
     @comment.post_id = @post.id
     @comment.save!
 
-    @comments = PostComment.where(reply_comment: @post_comment.id)
+    # reply_create.jsへ送る用 非同期通信
+    @comments = PostComment.where(reply_comment: @comment.reply_comment)
     @post_comment_n = PostComment.new
+    @post_comment = PostComment.find_by(id: @comment.reply_comment)
+  end
+  # 返信コメント削除
+  def reply_destroy
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+    @reply_comment = @post_comment.reply_comment
+    @comment = PostComment.find_by(id: @post_comment.id, post_id: @post.id)
+    @comment.destroy
+
+    # reply_create.jsへ送る用 非同期通信
+    @comments = PostComment.where(reply_comment: @reply_comment)
+    @post_comment_n = PostComment.new
+    @post_comment = PostComment.find_by(id: @reply_comment)
   end
 
   def edit
@@ -52,10 +68,12 @@ class Public::PostCommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-    @comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment = PostComment.find_by(id: params[:id], post_id: @post.id)
+    @reply_comment = PostComment.where(reply_comment: @comment.id)
     @comment.destroy
-    @comments = @post.post_comments
+    @reply_comment.destroy_all
 
+    @comments = PostComment.where(post_id: @post.id, reply_comment: nil)
     @post_comment_n = PostComment.new
   end
 
