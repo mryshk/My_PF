665ff9ef751e913895caeeff7775ac84a1a50d9d
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-07 14:14:20 +0000

    Add bookmark

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -183,14 +183,28 @@ $(document).on('turbolinks:load', function() {
 
 // マイページのタブ機能
 
-$(document).on('Turbolinks:load', function(){
-  $(function(){
-    $('.tab').click(function(){
-      $('.tab-active').removeClass('tab-active');
-      $(this).addClass('tab-active');
-      $('box-show').removeClass('box-show');
-      const index = $(this).index()
-      $('tabbox').eq(index).addClass('box-show');
-    });
+$(document).on('turbolinks:load',function(){
+  $('.tab').click(function(){
+    $('.tab-active').removeClass('tab-active');
+    $(this).addClass('tab-active');
+    $('.box-show').removeClass('box-show');
+    const index = $(this).index();
+    $('.tabbox').eq(index).addClass('box-show');
   });
 });
+
+// $(document).on('turbolinks:load',function() {
+//     // ↓タブをクリックした時の処理↓
+//     $('.tab').click(function(){
+//         // ↓現在アクティブなタブを切り替え↓
+//         $('.tab-active').removeClass('tab-active');
+//         // ↓タブをアクティブにする↓
+//         $(this).addClass('tab-active');
+//         // ↓現在アクティブなタブの中身を非表示
+//         $('.box-show').removeClass('box-show');
+//         // ↓クリックしたタブから順番を取得↓
+//         const index = $(this).index();
+//         // ↓クリックしたタブと同じ順番のコンテンツを表示↓
+//         $('.tabbox').eq(index).addClass('box-show');
+//     });
+// });
