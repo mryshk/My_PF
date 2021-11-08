98435e4c7f6f2900588a48141db4c0cfd597c1b2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-08 09:28:07 +0000

    Add action

--- app/controllers/public/reposts_controller.rb
+++ app/controllers/public/reposts_controller.rb
@@ -1,15 +1,22 @@
 class Public::RepostsController < ApplicationController
   before_action :set_post
-  
+
   def create
-    
+    if Repost.find_by(listener_id: current_listener.id, post_id: @post.id)
+      alert :"Already reposted"
+    else
+      @repost = Repost.create(listener_id: current_listener.id, post_id: @post.id)
+    end
+
   end
-  
+
   def destroy
-    
+    @repost = current_listener.reposts.find_by(post_id: @post.id)
+    @repost.destroy
   end
-  
+
   private
   def set_post
+    @post = Post.find(params[:post_id])
   end
 end
