f71dafd65ce28573d46b949f525a19ba69b7ad0c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-08 08:49:52 +0000

    add asociation

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -24,6 +24,7 @@ class Listener < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :post_favorites, dependent: :destroy
+  has_many :reposts, dependent: :destroy
 
   has_many :group_listeners, dependent: :destroy
   has_many :groups, through: :group_listeners, dependent: :destroy
