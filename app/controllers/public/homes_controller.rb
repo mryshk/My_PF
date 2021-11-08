66a32a6724889af7aaefa2361bbcfdf498e20590
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-08 15:14:57 +0000

    modify

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,16 +7,16 @@ class Public::HomesController < ApplicationController
 
   def home
     # フォローしている人のみを表示。タイムライン機能。
-    # following_ids = Relationship.select(:followed_id).where(follower_id: current_listener.id)
-    # repost_ids = Repost.select(:post_id).where(listener_id: [current_listener, *current_listener.following_ids])
-    # @posts = Post.where("id IN (#{repost_ids}}) OR listener_id IN (#{following_ids}})").page(params[:page]).per(2).reverse_order
-    @posts = Post.where(listener_id: [current_listener, *current_listener.following_ids]).page(params[:page]).includes(:listener).per(2).reverse_order
+    repost_ids = Repost.select(:post_id).where(listener_id: [current_listener, *current_listener.following_ids])
+    @posts = Post.where(id: repost_ids).or(Post.where(listener_id: [current_listener, *current_listener.following_ids])).page(params[:page]).includes(:listener).per(2).reverse_order
+    # @posts = Post.where(listener_id: [current_listener, *current_listener.following_ids]).page(params[:page]).includes(:listener).per(2).reverse_order
     @post_favorite_rank = Post.includes(:favo_users).sort { |a, b| b.favo_users.size <=> a.favo_users.size }
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
     # メニュー用
     # 自分の所属するグループを全て集める。
     mygroup_ids = current_listener.group_listeners.pluck(:group_id)
     @mygroups = Group.where(id: mygroup_ids)
+
   end
 
   # def post_join
