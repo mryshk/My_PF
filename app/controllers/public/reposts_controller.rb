9001d32d76d7b7fa64e92b35df245609759ebe3f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-08 08:56:50 +0000

    Add action

--- app/controllers/public/reposts_controller.rb
+++ app/controllers/public/reposts_controller.rb
@@ -1,2 +1,15 @@
 class Public::RepostsController < ApplicationController
+  before_action :set_post
+  
+  def create
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  def set_post
+  end
 end
