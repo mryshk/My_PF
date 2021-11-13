45d8b6b84a4cd392f6c1a0b0b8310f6fac12a4e2
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-13 03:27:50 +0000

    modify

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,30 +1,27 @@
 class Public::HomesController < ApplicationController
-  # before_action :post_join
+  before_action :set_menu,only:[:home_post,:home_album]
 
 
   def top
   end
 
-  def home
+  def home_post
     # フォローしている人のみを表示。タイムライン機能。
-    # following_repost_ids = Repost.where(listener_id: [current_listener, *current_listener.following_ids]).distinct.pluck(:post_id)
-    # my_following_ids = Relationship.select(:followed_id).where(follower_id: current_listener.id).distinct.pluck(:followed_id)
-    # @post_repost = Post.joins("LEFT OUTER JOIN reposts ON post_id = reposts.post_id AND (reposts.listener_id = #{current_listener.id} OR reposts.listener_id IN (SELECT followed_id FROM relationships WHERE follower_id = #{current_listener.id}))").select("posts.*,reposts.listener_id AS repost_listener_id, (SELECT name FROM listeners WHERE id = reposts.listener_id) AS repost_listener_name")
-    # @posts = @post_repost.where(listener_id: my_following_ids).or(@post_repost.where(id: following_repost_ids)).page(params[:page]).per(10).reverse_order
-    # @posts = Post.where(id: following_repost_ids).or(Post.where(listener_id: [current_listener, *current_listener.following_ids])).page(params[:page]).includes(:listener).per(10).reverse_order
-    # @posts = Post.where(listener_id: [current_listener, *current_listener.following_ids]).page(params[:page]).includes(:listener).per(2).reverse_order
-
     @listener = Listener.find(current_listener.id)
     @posts = @listener.followings_posts_with_reposts.page(params[:page]).per(10).reverse_order
     @post_favorite_rank = Post.includes(:favo_users).sort { |a, b| b.favo_users.size <=> a.favo_users.size }
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
+  end
+
+  def home_album
+    @albums = Album.where(listener_id: [current_listener, *current_listener.following_ids]).page(params[:page]).includes(:listener).per(2).reverse_order
+  end
+
+  def set_menu
     # メニュー用
     # 自分の所属するグループを全て集める。
     mygroup_ids = current_listener.group_listeners.pluck(:group_id)
     @mygroups = Group.where(id: mygroup_ids)
   end
 
-  # def post_join
-  #
-  # end
 end
