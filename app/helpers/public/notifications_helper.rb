a586e0baffbfd9313e36b798bdbf2a417ca710a5
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-14 03:15:19 +0000

    modify comment_nortification

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -9,11 +9,10 @@ module Public::NotificationsHelper
     when "follow" then
       tag.a(notification.active.name, href: listener_path(@visiter), class:"notification_text link p-0") + "があなたをフォローしました。"
     when "like" then
-      tag.a(notification.active.name, href: listener_path(@visiter), class:"notification_text link p-0")+ "が" + tag.a('あなた投稿を', href: post_path(notification.post_id),class:"notification_text link p-0")+ "いいねしました。"
+      tag.a(notification.active.name, href: listener_path(@visiter), class:"notification_text link p-0") + "が" + tag.a('あなた投稿を', href: post_path(notification.post_id),class:"notification_text link p-0") + "いいねしました。"
     when "comment" then
       @comment = PostComment.find_by(id: @active_comment)&.comment
-      tag.a(@visiter.name, href: listener_path(@visiter),class:"notification_text link p-0")
-      + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id),class:"notification_text link p-0") + "にコメントしました"
+      tag.a(@visiter.name, href: listener_path(@visiter),class:"notification_text link p-0") + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id),class:"notification_text link p-0") + "にコメントしました"
     end
   end
 
