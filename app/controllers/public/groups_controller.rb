05dda33928189f439ef6c929492253e007bc1ecd
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-14 02:33:14 +0000

    Add reverse_order_to_group

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -18,7 +18,7 @@ class Public::GroupsController < ApplicationController
   end
 
   def index
-    @groups = Group.page(params[:page]).per(2)
+    @groups = Group.page(params[:page]).per(2).reverse_order
     @group_listeners_rank = Group.includes(:listeners).sort { |a, b| b.listeners.size <=> a.listeners.size }
 
     # メニュー用
