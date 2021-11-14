192e003f780efc5b3beae0bc3915ba8c4a6c9a85
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-14 02:29:45 +0000

    modify per()

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -8,7 +8,7 @@ class Public::HomesController < ApplicationController
   def home_post
     # フォローしている人のみを表示。タイムライン機能。
     @listener = Listener.find(current_listener.id)
-    @posts = @listener.followings_posts_with_reposts.page(params[:page]).per(10).reverse_order
+    @posts = @listener.followings_posts_with_reposts.page(params[:page]).per(4).reverse_order
     @post_favorite_rank = Post.includes(:favo_users).sort { |a, b| b.favo_users.size <=> a.favo_users.size }
     @post_impression_rank = Post.all.order(impressions_count: 'DESC').page(params[:page])
   end
