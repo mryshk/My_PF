5d6bfe231c899824cb8e826fd1c6e06408f7f325
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-15 02:37:31 +0000

    modified by rubocop

--- db/migrate/20211108071958_create_reposts.rb
+++ db/migrate/20211108071958_create_reposts.rb
@@ -1,7 +1,7 @@
 class CreateReposts < ActiveRecord::Migration[5.2]
   def change
     create_table :reposts do |t|
-      t.integer :listener,foreign_key: true
+      t.integer :listener, foreign_key: true
       t.integer :post, foreign_key: true
       t.timestamps
     end
