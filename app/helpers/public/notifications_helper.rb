f5b19880552846c7c16a7f33232d1ecefd87248b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-15 02:36:39 +0000

    modified by rubocop

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -7,12 +7,12 @@ module Public::NotificationsHelper
 
     case notification.action
     when "follow" then
-      tag.a(notification.active.name, href: listener_path(@visiter), class:"notification_text link p-0") + "があなたをフォローしました。"
+      tag.a(notification.active.name, href: listener_path(@visiter), class: "notification_text link p-0") + "があなたをフォローしました。"
     when "like" then
-      tag.a(notification.active.name, href: listener_path(@visiter), class:"notification_text link p-0") + "が" + tag.a('あなた投稿を', href: post_path(notification.post_id),class:"notification_text link p-0") + "いいねしました。"
+      tag.a(notification.active.name, href: listener_path(@visiter), class: "notification_text link p-0") + "が" + tag.a('あなた投稿を', href: post_path(notification.post_id), class: "notification_text link p-0") + "いいねしました。"
     when "comment" then
       @comment = PostComment.find_by(id: @active_comment)&.comment
-      tag.a(@visiter.name, href: listener_path(@visiter),class:"notification_text link p-0") + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id),class:"notification_text link p-0") + "にコメントしました"
+      tag.a(@visiter.name, href: listener_path(@visiter), class: "notification_text link p-0") + "が" + tag.a('あなたの投稿', href: post_path(notification.post_id), class: "notification_text link p-0") + "にコメントしました"
     end
   end
 
