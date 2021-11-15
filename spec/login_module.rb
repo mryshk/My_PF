42bd75c890b08362f581fed33f85cf677cfc8151
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-15 02:37:49 +0000

    modified by rubocop

--- spec/login_module.rb
+++ spec/login_module.rb
@@ -3,7 +3,7 @@ module LoginModule
     visit new_listener_session_path
     fill_in 'email', with: listener.email
     fill_in 'password', with: "password"
-    fill_in 'password_confirmation',with: "password"
+    fill_in 'password_confirmation', with: "password"
     click_button 'LogIn'
   end
 end
