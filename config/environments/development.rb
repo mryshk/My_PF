a56ef8fb5dce047825fa48a70a70c2288040d7bf
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-15 02:36:55 +0000

    modified by rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,13 +59,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      port: 587,
-      address: 'smtp.gmail.com',
-      domain: 'smtp.gmail.com',
-      user_name: ENV['SMTP_USERNAME'],
-      password: ENV['SMTP_PASSWORD'],
-      enable_starttls_auto: true
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'smtp.gmail.com',
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    enable_starttls_auto: true,
   }
 
   # config.after_initialize do
