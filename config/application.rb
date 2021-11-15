3c92cf506db2986cee130807f53e634e67caae7b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-15 02:36:51 +0000

    modified by rubocop

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Music
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.precompile += %w( admin/active_admin.scss )
-    config.assets.precompile += %w( admin/active_admin.js )
+    config.assets.precompile += %w(admin/active_admin.scss)
+    config.assets.precompile += %w(admin/active_admin.js)
   end
 end
