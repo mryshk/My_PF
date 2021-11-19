11e2d77bef82c5631b6564a1331386937529c473
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-19 01:17:35 +0000

    modified by rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_11_18_121747) do
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -251,5 +250,4 @@ ActiveRecord::Schema.define(version: 2021_11_18_121747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
