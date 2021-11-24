9fa8291753bccb35c1389b6df4c3c998809f946f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-24 12:07:20 +0000

    modify validate

--- app/assets/javascripts/jqueryconfig.js
+++ app/assets/javascripts/jqueryconfig.js
@@ -7,11 +7,17 @@ $(function(){
         required: true,
         maxlength: 140
       },
+      "post[post_url]": {
+        required: true,
+      }
     },
     messages: {
       "post[post_tweet]": {
         required: "投稿文を入力してください。"
       },
+      "post[post_url]": {
+        required: "音楽のURL（Youtubeや音楽サブスク等）を入力してください。"
+      }
     },
     errorClass: "invalid",
     errorElement: "p",
