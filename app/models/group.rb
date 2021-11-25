90307c0993aa884302d151dcb9fd1ef24183ff65
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-25 01:23:18 +0000

    Add comment

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,16 @@
 class Group < ApplicationRecord
+  # 画像投稿のため。
   attachment :image
+
+  # アソシエーション
   has_many :group_listeners, dependent: :destroy
   has_many :group_chats, dependent: :destroy
   has_many :listeners, through: :group_listeners, dependent: :destroy
+
+  # グループ作成のバリデーション
   validates :name, presence: true
 
+  # グループに参加しているかどうかの確認。
   def joined_by?(listener_id)
     group_listeners.where(listener_id: listener_id).exists?
   end
