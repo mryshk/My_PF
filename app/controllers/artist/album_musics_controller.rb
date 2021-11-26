ff72d9991ebb2488b5dcf4c244e4e32a61953f04
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-26 03:22:15 +0000

    Add ensure_listener_action

--- app/controllers/artist/album_musics_controller.rb
+++ app/controllers/artist/album_musics_controller.rb
@@ -1,4 +1,7 @@
 class Artist::AlbumMusicsController < ApplicationController
+
+  # 本人確認。 自分以外の人をアクセス不可にするための確認。
+  before_action :ensure_listener,only:[:edit,:update,:destroy]
   # 権限確認（cancancan）
   authorize_resource
 
@@ -34,13 +37,9 @@ class Artist::AlbumMusicsController < ApplicationController
   end
 
   def edit
-    @album = Album.find_by(id: params[:album_id])
-    @album_music = AlbumMusic.find(params[:id])
   end
 
   def update
-    @album = Album.find_by(id: params[:album_id])
-    @album_music = AlbumMusic.find_by(album_id: params[:album_id], id: params[:id])
     @album_music.listener_id = current_listener.id
     if @album_music.update(album_music_params)
       redirect_to artist_album_album_music_path(@album, @album_music)
@@ -50,13 +49,20 @@ class Artist::AlbumMusicsController < ApplicationController
   end
 
   def destroy
-    @album = Album.find_by(id: params[:album_id])
-    @album_music = AlbumMusic.find_by(album_id: @album.id, id: params[:id])
     @album_music.destroy
     redirect_to artist_album_path(@album)
   end
 
   private
+
+  # 本人確認 本人以外であれば、ホーム画面へ遷移。
+  def ensure_listener
+    @album = Album.find_by(id: params[:album_id])
+    @album_music = AlbumMusic.find_by(album_id: @album.id, id: params[:id])
+    if @album_music.listener_id != current_listener.id
+      redirect_to home_post_path, alert: '画面を閲覧する権限がありません。'
+    end
+  end
 
   def album_music_params
     params.require(:album_music).permit(:name, :caption, :music_url, :listener_id)
