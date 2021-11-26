35ef408b7db7f9612949fd53d518af8ba8a01168
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-26 03:22:21 +0000

    Add ensure_listener_action

--- app/controllers/public/listeners_controller.rb
+++ app/controllers/public/listeners_controller.rb
@@ -1,11 +1,9 @@
 class Public::ListenersController < ApplicationController
-  before_action :set_listener
-
-  def set_listener
-    @listener = Listener.find(params[:id])
-  end
+  # 本人確認。 自分以外の人をアクセス不可にするための確認。
+  before_action :ensure_listener,only:[:edit,:update]
 
   def show
+    @listener = Listener.find(params[:id])
     @posts = @listener.posts.reverse_order
     @bookmarks = PostFavorite.where(listener_id: @listener.id).includes(:post).reverse_order
     @reposts = Repost.where(listener_id: @listener.id).includes(:post).reverse_order
@@ -20,6 +18,14 @@ class Public::ListenersController < ApplicationController
   end
 
   private
+
+  # 本人確認 本人以外であれば、ホーム画面へ遷移。
+  def ensure_listener
+    @listener = Listener.find(params[:id])
+    if @listener.id != current_listener.id
+      redirect_to home_post_path, alert: '画面を閲覧する権限がありません。'
+    end
+  end
 
   def listener_params
     params.require(:listener).permit(:name, :caption, :profile_image, :profile_back_image, :listener_type, :email)
