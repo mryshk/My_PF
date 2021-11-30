3ddd22ad4c69a0cc89e2257865e2ed78d19e1039
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-30 07:38:30 +0000

    Add album_music_validation

--- spec/models/album_music_spec.rb
+++ spec/models/album_music_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe AlbumMusic, "モデルに関するテスト", type: :model do
   end
   describe "バリデーションテスト"do
     subject { album_music.valid? }
-    
+
     let!(:listener) { create(:listener) }
     let!(:album_music) { build(:album_music, listener_id: listener.id) }
-    
+
     context "nameカラムの確認" do
       it "空白であるかどうかの確認" do
         album_music.name = ""
@@ -25,10 +25,14 @@ RSpec.describe AlbumMusic, "モデルに関するテスト", type: :model do
         album_music.caption = Faker::Lorem.characters(number: 140)
         is_expected.to eq true
       end
-    end
-    context "music_urlカラムの確認" do
       it "140文字以上である場合：140文字以上であればFalse" do
         album_music.caption = Faker::Lorem.characters(number: 200)
+        is_expected.to eq false
+      end
+    end
+    context "music_urlカラムの確認" do
+      it "空白であるかの確認" do
+        album_music.music_url = ""
         is_expected.to eq false
       end
     end
