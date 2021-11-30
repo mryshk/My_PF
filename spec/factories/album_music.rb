f6ad06fefe0d6ba9c9e21e436a52ffd673f40c0f
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-30 07:36:42 +0000

    Add album_music_validation

--- spec/factories/album_music.rb
+++ spec/factories/album_music.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :album_music do
     association :creater
+    association :listener
     association :album
     name { Faker::Name.name }
     caption { Faker::Lorem.characters(number: 50) }
--- spec/models/album_music_spec.rb
+++ spec/models/album_music_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe AlbumMusic, "モデルに関するテスト", type: :model do
       expect(FactoryBot.build(:album_music)).to be_valid
     end
   end
+  describe "バリデーションテスト"do
+    subject { album_music.valid? }
+    
+    let!(:listener) { create(:listener) }
+    let!(:album_music) { build(:album_music, listener_id: listener.id) }
+    
+    context "nameカラムの確認" do
+      it "空白であるかどうかの確認" do
+        album_music.name = ""
+        is_expected.to eq false
+      end
+    end
+    context "captionカラムの確認" do
+      it "140文字以下の場合：140字以下であればTrue" do
+        album_music.caption = Faker::Lorem.characters(number: 140)
+        is_expected.to eq true
+      end
+    end
+    context "music_urlカラムの確認" do
+      it "140文字以上である場合：140文字以上であればFalse" do
+        album_music.caption = Faker::Lorem.characters(number: 200)
+        is_expected.to eq false
+      end
+    end
+  end
 end
