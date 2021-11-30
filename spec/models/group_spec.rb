a93203ede06e0fe38b27117604b920cd930c82bc
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-11-30 07:47:09 +0000

    Add group_validation_test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -10,4 +10,27 @@ RSpec.describe Group, "モデルに関するテスト", type: :model do
       expect(group).to be_valid
     end
   end
+  describe "バリデーション確認" do
+    subject { group.valid? }
+
+    let!(:listener) { create(:listener) }
+    let!(:group) { build(:group, owner_id: listener.id) }
+
+    context "namaカラムの確認" do
+      it "空白であるかの確認" do
+        group.name = ""
+        is_expected.to eq false
+      end
+    end
+    context "introductionカラムの確認" do
+      it "140文字以下の場合：140文字以下であればTrue" do
+        group.introduction = Faker::Lorem.characters(number: 140)
+        is_expected.to eq true
+      end
+      it "140文字以上の場合：140文字以上であればFalse" do
+        group.introduction = Faker::Lorem.characters(number: 200)
+        is_expected.to eq false
+      end
+    end
+  end
 end
